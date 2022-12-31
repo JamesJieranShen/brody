7f0295c80dda13b6273eb2057d818542492fef04
James Shen <jamesjieranshen@gmail.com>
2022-12-31 04:01:54 -0500

    2d NLL

--- reconstruction/likelihood.py
+++ reconstruction/likelihood.py
@@ -1,9 +1,13 @@
+from typing import Callable
 import numpy as np
 from scipy.optimize import curve_fit
-from scipy.interpolate import RectBivariateSpline
+from scipy.interpolate import interp2d
 import matplotlib.pyplot as plt
 
 from .utils import polyform
+import logging
+from brody.log import logger
+
 
 class LikelihoodException(Exception):
     pass
@@ -57,13 +61,16 @@ class NLL1D(object):
         if pull is None:
             npts = min(len(self.nll) // 2, 10)
             # fmt: off
-            left_pull  = -np.mean((self.nll[:npts-1]-self.nll[1:npts])/(self.centers[:npts-1]-self.centers[1:npts]))
-            right_pull =  np.mean((self.nll[-npts:-1]-self.nll[-npts+1:])/(self.centers[-npts:-1]-self.centers[-npts+1:]))
-            self.pull  =  (left_pull, right_pull)
+            left_pull = -np.mean((self.nll[:npts-1]-self.nll[1:npts]) /
+                                 (self.centers[:npts-1]-self.centers[1:npts]))
+            right_pull = np.mean(
+                (self.nll[-npts: -1] - self.nll[-npts + 1:]) /
+                (self.centers[-npts: -1] - self.centers[-npts + 1:]))
+            self.pull = (left_pull, right_pull)
             # fmt: on
         else:
             self.pull = (pull, pull)
-            
+
         self._call_fn = np.interp
         self._call_args = (self.centers, self.nll)
 
@@ -91,11 +98,12 @@ class NLL1D(object):
     def mean(self):
         weights = np.exp(-self.nll)
         return np.sum(self.centers * weights) / np.sum(weights)
-    
+
     def plot(self):
         fig = plt.figure(figsize=(8, 6))
         ax = fig.add_subplot(111)
-        ax.plot(self.centers, self.nll, c='k', lw=0.5, label='data', zorder=-999)
+        ax.plot(self.centers, self.nll, c='k',
+                lw=0.5, label='data', zorder=-999)
         ax.plot(self.centers, self(self.centers), c='r', lw=0.5, label='fit')
         ax.set_xlabel("$x$")
         ax.set_ylabel("-log(likelihood)")
@@ -103,8 +111,6 @@ class NLL1D(object):
         ax.legend(frameon=False)
         ax.set_title(self.__class__.__name__)
         return ax
-
-
 
 
 class CosAlphaNLL(NLL1D):
@@ -120,269 +126,60 @@ class CosAlphaNLL(NLL1D):
         poly_right = np.zeros(order)
         poly_left = np.zeros(order)
 
-        p, __ = curve_fit(polyform, self.centers, self.nll, p0=np.concatenate([[cher_peak], poly_left, poly_right]))
+        p, __ = curve_fit(polyform, self.centers, self.nll, p0=np.concatenate(
+            [[cher_peak], poly_left, poly_right]))
 
         self._call_fn = polyform
         self._call_args = p
 
+
 class NLL2D(object):
-    """
-    A callable object that evaluates the -log(likelihood) values given two axes of some binned data
-    Does a few nonstandard things:
-        - Forces a 'min_val' for each bin to avoid -log(0) to make low stats behave and avoid NaNs
-        - Rolls off the probability at PDF boundaries to 'pull' outliers into the defined range
-            (probability e^(-Ax) with x being distance from pdf boundary and A being 'pull').
+    cosalpha_edges: np.ndarray
+    cosalpha_centers: np.ndarray
+    tresid_edges: np.ndarray
+    nll_values: np.ndarray
+    interp_fn: Callable
+    pull: float
 
-    Some nonstandard variables:
-    - pull_axes: determines which axes whereby probabilities are rolled off via single string
-        containing x and/or y or neither. Only supports axes, and not individual sides of the PDF.
-    - pullsize: small, medium, or large. Determines maximum percentage of each axis that can be
-                rolled off. 10% for small, 16% for medium, 20% for large.
-    - verbose: saves a plot of the evaluated NLL.
-    - pull: if specificed, is [xpull,ypull]
+    def __init__(
+            self, counts: np.ndarray, cosalpha_edges: np.ndarray, tresid_edges: np.ndarray,
+            pull=None) -> None:
+        # X is cosAlpha, Y is tresid
+        assert counts.shape == (len(cosalpha_edges) - 1, len(tresid_edges) - 1)
+        self.cosalpha_edges = cosalpha_edges
+        self.tresid_edges = tresid_edges
+        # Assume the counts are un-normalized, set all zeros to 1, and then normalilze
+        if logger.level >= logging.WARNING:
+            N_zeroBins = np.sum(counts == 0)
+            if N_zeroBins > 0:
+                logger.warning(f"Found {N_zeroBins} zero bins in 2D NLL, setting them to 1.")
+        counts = np.where(counts == 0, 1, counts)
+        counts /= np.sum(counts)
+        self.nll_values = -np.log(counts / np.sum(counts))
+        # Create interpolation function
+        tresid_centers = (tresid_edges[:-1] + tresid_edges[1:]) / 2
+        self.cosalpha_centers = (cosalpha_edges[:-1] + cosalpha_edges[1:]) / 2
+        self.interp_fn = interp2d(self.cosalpha_centers, tresid_centers,
+                                  self.nll_values, kind='linear')
+        if pull is None:
+            # By default, use bin width as pull distance
+            self.pull = tresid_edges[1] - tresid_edges[0]
 
-    If you don't specify a min_val, this will guess one as being 100 times less than the bin
-    with the most counts.
-
-    If you don't specify a pull, this will guess one by calculating the average A using at most
-    10 points on each side of the data range passing the min_val cut.
-
-    Note that the "counts" and "nll" arrays are treated as such:
-
-    nll[0, -1] ------------------- nll[-1, -1]
-              |                  |     ^           ycenters[-1]
-              |                  |     |
-              |    NLL  ARRAY    | ycenters
-              |                  |     |
-              |                  |     v           ycenters[0]
-    nll[0, 0] ------------------- nll[0, -1]
-               <--- xcenters --->
-
-     xcenters[0]                xcenters[-1]
-
-    where xcenters = (xedges[1:]+xedges[:-1])/2
-    and likewise for ycenters
-
-    """
-
-    def __init__(self):
-        return
-    #     self,
-    #     counts,
-    #     xedges,
-    #     yedges,
-    #     min_val=None,
-    #     pull_axes="",
-    #     pull=None,
-    #     verbose=False,
-    #     s=0,
-    # ):
-    #     """
-    #     1. Get data ready for manipulation
-    #     """
-    #     # hard edges
-    #     xs = (xedges[:-1] + xedges[1:]) / 2
-    #     ys = (yedges[:-1] + yedges[1:]) / 2
-    #     self.xmin, self.xmax = xs[0], xs[-1]
-    #     self.ymin, self.ymax = ys[0], ys[-1]
-
-    #     # allow for capitals in pull_axes
-    #     pull_axes = pull_axes.lower() if isinstance(pull_axes, str) else ""
-
-    #     # length - 1 because we're dealing with edges and not centers
-    #     # if for some reason the 2D counts array was transposed before calling NLLPDF_2D, this takes care of that.
-    #     if len(counts) != len(xedges) - 1:
-    #         counts = counts.T
-
-    #     # copy counts to a new array and find the lengths of x and y axes
-    #     self.nll = counts.copy()
-    #     # find min_val
-    #     if min_val is None and np.min(self.nll) == 0:
-    #         min_val = np.max(self.nll) / 100
-    #     elif np.min(self.nll) != 0:
-    #         min_val = np.min(self.nll)
-
-    #     # xmin, xmax, ymin, ymax = self.determine_edges(min_val, pull_axes, pullsize)
-    #     xmin, xmax, ymin, ymax = 0, -1, 0, -1
-
-    #     """
-    #     3. Convert to NLL and centers along each axis within the pull bounds, and plot it (if specified)
-    #     """
-    #     sample_count = np.sum(self.nll)
-    #     bin_area = (xedges[1] - xedges[0]) * (yedges[1] - yedges[0])  # ASSUMES UNIFORM BIN AREAS!
-    #     norm = sample_count * bin_area
-    #     bbox = [min(xs), max(xs), min(ys), max(ys)]
-
-    #     self.nll = RectBivariateSpline(xs, ys, self.nll / norm, bbox, kx=1, ky=1, s=0.0)(xs, ys)
-    #     self.nll[self.nll < min_val / norm] = min_val / norm
-    #     self.nll = -np.log(self.nll[xmin:xmax, ymin:ymax])
-    #     self.min_val = np.min(self.nll)
-    #     self.xcenters = xs[xmin:xmax]
-    #     self.ycenters = ys[ymin:ymax]
-    #     """
-    #     4. Calculate pull along each side of the PDF.
-    #     """
-    #     self.pull = self.determine_pull(xmin, xmax, ymin, ymax) if pull is None else pull
-
-    #     """ 
-    #     (5.) Optionally fit the cos[theta] distribution in each time residual bin, sample the distribution, and set that as the
-    #     NLL.
-    #     """
-    #     if polyform_fit:
-
-    #         order = 5
-
-    #         for idx, x in enumerate(self.xcenters):
-    #             if abs(x) < 10:
-    #                 nll = self.nll[idx, :]
-
-    #                 cher_peak = self.ycenters[np.argmin(nll)]
-    #                 poly_right = np.zeros(order)
-    #                 poly_left = np.zeros(order)
-
-    #                 p, p_cov = curve_fit(
-    #                     polyform,
-    #                     self.ycenters,
-    #                     nll,
-    #                     p0=np.concatenate([[cher_peak], poly_left, poly_right]),
-    #                     maxfev=100000,
-    #                 )
-    #                 self.nll[idx, :] = polyform(self.ycenters, *p)
-
-    #     """
-    #     scipy.interpolate.RectBivariateSpline is a faster version of scipy.interpolate.interp2d
-    #     Must switch y's and x's for this class (mirrors some MATLAB stuff)... or not? I suppose
-    #     the transposed counts array at the start fixes this.
-    #     kx and ky are polynomial powers; 1 for linear (what we want)
-    #     s denotes smoothing -- we don't like smoothing
-    #     """
-
-    #     rbf = RectBivariateSpline(self.xcenters, self.ycenters, self.nll)
-    #     self.interp = rbf
-    #     # self.interp = interp2d(self.xcenters, self.ycenters, self.nll.T)
-
-    #     if verbose:
-    #         self.debug_plot(self.nll)
-
-    # def determine_edges(self, min_val, pull_axes, pullsize):
-
-    #     # find pull bounds (will be modified if needed later)
-    #     pull_bounds = np.argwhere(self.nll > min_val)
-    #     """
-    #     2. Find top left, top right, bottom left, and bottom right indices
-    #        of the pull_bounds mask.
-    #        This traces out the largest possible rectangle whereby the value
-    #        of each indice is True.
-        
-    #     """
-    #     xlength = len(self.nll)
-    #     ylength = len(self.nll[0])
-
-    #     bottom_left = pull_bounds[0]
-    #     top_right = pull_bounds[-1]
-
-    #     # create a mask that returns the leftmost column.
-    #     max_bottom_left = pull_bounds[:, 0] == bottom_left[0]
-    #     # find last element of 1st column -- yields args of top-left point
-    #     top_left = pull_bounds[max_bottom_left][-1]
-
-    #     # create a mask that returns the rightmost column.
-    #     max_bottom_right = pull_bounds[:, 0] == top_right[0]
-    #     # find last element of 1st column -- yields args of top-right point
-    #     bottom_right = pull_bounds[max_bottom_right][0]
-
-    #     # take care of pull_axes, while ensuring pull_axes' requirements
-    #     xmin = max(bottom_left[0], top_left[0]) if "x" in pull_axes else 0
-    #     xmax = min(bottom_right[0], top_right[0]) if "x" in pull_axes else xlength - 1
-    #     ymin = max(bottom_left[1], bottom_right[1]) if "y" in pull_axes else 0
-    #     ymax = min(top_left[1], top_right[1]) if "y" in pull_axes else ylength - 1
-    #     """
-    #     We don't want to roll off more than ~10% of of binning on a given axis (5% above, 5% below),
-    #     possibly getting rid of important structure.
-        
-    #     If the cut data outside of the pull_bounds exceeds this percentage of the array, the cut data
-    #     is decreased to at most 5%, 8% or 10% on each side of the PDF.
-    #     """
-    #     max_difs = {
-    #         "small": 0.05,
-    #         "mid": 0.08,
-    #         "large": 0.10,
-    #     }  # to convert from string to size
-    #     max_dif = max_difs[pullsize]  # in percentage
-    #     if (ylength - ymax) > max_dif * ylength:
-    #         ymax = round((1 - max_dif) * (ylength - 1))
-    #     if (ymin) > max_dif * (ylength - 1):
-    #         ymin = round(max_dif * (ylength - 1))
-    #     if (xlength - xmax) > max_dif * xlength:
-    #         xmax = round((1 - max_dif) * (xlength - 1))
-    #     if (xmin) > max_dif * (xlength - 1):
-    #         xmin = round(max_dif * (xlength - 1))
-
-    #     # Make sure we have enough binning in the first place
-    #     if (xmax - xmin) < 20:
-    #         raise Exception(
-    #             "Too few bins remaining for a good PDF in X-dir (bin finer?); %i - %i = %i"
-    #             % (xmax, xmin, xmax - xmin)
-    #         )
-    #     if (ymax - ymin) < 20:
-    #         raise Exception(
-    #             "Too few bins remaining for a good PDF in Y-dir (bin finer?); %i - %i = %i"
-    #             % (ymax, ymin, ymax - ymin)
-    #         )
-
-    #     return xmin, xmax, ymin, ymax
-
-    # def determine_pull(self, xmin, xmax, ymin, ymax):
-    #     # is this the best?
-    #     nptsy = min((ymax - ymin) // 2, 10)
-    #     nptsx = min((xmax - xmin) // 2, 10)
-
-    #     # find dx/dy for slope calculation
-    #     dx_l = (self.xcenters)[: nptsx - 1] - (self.xcenters)[1:nptsx]
-    #     dx_r = (self.xcenters)[-nptsx + 2 :] - (self.xcenters)[-nptsx:-2]
-    #     dy_b = (self.ycenters)[: nptsy - 1] - (self.ycenters)[1:nptsy]
-    #     dy_t = (self.ycenters)[-nptsy + 2 :] - (self.ycenters)[-nptsy:-2]
-
-    #     # define left pull
-    #     left_domain = self.nll[:nptsx, :]
-    #     left_rows = np.column_stack(left_domain)
-    #     left_pull = [-np.mean((row[:-1] - row[1:]) / dx_l) for row in left_rows]
-
-    #     # define right pull
-    #     right_domain = self.nll[-nptsx + 1 :, :]
-    #     right_rows = np.column_stack(right_domain)
-    #     right_pull = [np.mean((row[:-1] - row[1:]) / dx_r) for row in right_rows]
-
-    #     # define bottom pull
-    #     bottom_domain = bottom_rows = self.nll[:, :nptsy]
-    #     bottom_pull = [-np.mean((row[:-1] - row[1:]) / dy_b) for row in bottom_rows]
-
-    #     # define top pull
-    #     top_domain = top_rows = self.nll[:, -nptsy + 1 :]
-    #     top_pull = [np.mean((row[:-1] - row[1:]) / dy_t) for row in top_rows]
-
-    #     return list(
-    #         map(np.asarray, [left_pull, right_pull, bottom_pull, top_pull])
-    #     )  # convert each list to a numpy array
-
-    # def __call__(self, x, y):
-
-    #     # calculate NLL at each inputted point (usually 0-4 points depending on simulation)
-    #     return self.interp(x, y, grid=False)
-
-    # def debug_plot(self, nll):
-    #     fig = plt.figure()
-    #     ax = fig.add_subplot(111, title="NLL")
-    #     im = ax.imshow(
-    #         self.interp(self.xcenters, self.ycenters).T,
-    #         aspect="auto",
-    #         origin="lower",
-    #         extent=[self.xmin, self.xmax, self.ymin, self.ymax],
-    #     )
-    #     plt.colorbar(im, ax=ax)
-    #     ax.set_xlabel(r"cos[$\alpha$]")
-    #     ax.set_ylabel("$t_{resid}$")
-    #     plt.show()
-    #     plt.close("all")
-
-    #     return
+    def __call__(self, cosAlpha, tresid):
+        # NOTE: Vectorization needs to be guaranteed for this function
+        assert (cosAlpha >= -1) and (cosAlpha <= 1)
+        value = self.interp_fn(cosAlpha, tresid)
+        value = np.where(tresid < self.tresid_edges[0],
+                         np.interp(cosAlpha, self.cosalpha_centers, self.nll_values[:, 0]),
+                         value)
+        value = np.where(tresid > self.tresid_edges[-1],
+                         np.interp(cosAlpha, self.cosalpha_centers, self.nll_values[:, -1]),
+                         value)
+        distance_to_edge = np.where(
+            tresid < self.tresid_edges[0],
+            self.tresid_edges[0] - tresid, 0) + np.where(
+            tresid > self.tresid_edges[-1],
+            tresid - self.tresid_edges[-1], 0)
+        assert np.all(distance_to_edge >= 0)  # Sanity check, can remove
+        value += distance_to_edge * self.pull
+        return value
